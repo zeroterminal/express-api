60ddbbd2df55140a956ff1b8bdf0fa24a1134c8c
Jad <zerolinuxx@gmail.com>
2023-09-06 04:19:52 -0400

    minor updates

--- index.js
+++ index.js
@@ -1,23 +1,37 @@
 import express from 'express';
 import usersRoutes from './routes/users.js';
 import mongoose from 'mongoose';
-import UsersSchema from './Models/schema.js';
+import Users from './Models/usersModel.js';
 import 'dotenv/config';
 
-mongoose.connect(`${process.env.database}`, { useNewUrlParser: true })
-const db = mongoose.connection
-// db.on('error', (error) => console.error(error))
-// db.once('open', () => console.error(error))
 
+const db = mongoose.connection
+db.on('error', console.error.bind(console, 'connection error:'));
+db.on('open', () => { console.log(' ...:: database opened successfully ::...')} )
 
 const app   = express();
 const PORT  = 5000;
-
 
 app.use(express.json());
 
 app.use('/users', usersRoutes);
 
 app.get('/', (req, res) => { res.send("homepage"); })
+
+mongoose.connect(process.env.database).then(() => {console.log("....:: Connected to Database ::...")}).catch((err) => {console.log(err)});
+
+
+console.log("\n\n *** " + Users.Username + " *** \n\n")
+
+// let isa = new Users(
+//     {
+//         Username: "aizashi",
+//         Name: "Meowabelle",
+//         Email: "little_farter@gmail.com",
+//         Password: "imeow@you"
+//     }
+// );
+    
+// isa.save(function(err, users) { if (err) { return console.error(err) } console.log(isa.name + " Saved to database") })
 
 app.listen(PORT, () => console.log(`server running on ${PORT}`));
--- routes/users.js
+++ routes/users.js
@@ -1,7 +1,7 @@
 import express from 'express';
 const router = express.Router();
 
-router.get('/',  (req, res) => { res.send('Hello'); });
-router.post('/', (req, res) => { res.send('Hello'); });
+router.get('/',  (req, res) => { res.send('Getted /users'); });
+router.post('/', (req, res) => { res.send('Posted /users'); });
 
 export default router;
